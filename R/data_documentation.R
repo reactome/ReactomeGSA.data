1de570b6ddfe9748175e13ffa1e638c80690f2c3
Johannes Griss <jgriss8@yahoo.com>
2019-04-02 14:07:53 +0100

    Initial commit

---
+++ R/data_documentation.R
@@ -0,0 +1,20 @@
+#' Proteomics intensity-based quantitation data from the B-Cell
+#' melanoma induction study by Griss et al.
+#'
+#' The data is available as a \code{\link[limma]{EList}} object containing
+#' the aggregated protein intensity values. Normalisation was already performed
+#' on the PSM level prior to protein-level aggregation.
+#'
+#' @author Johannes Griss \email{johannes.griss@meduniwien.ac.at}
+#' @references Griss et al., bioRxiv 478735; doi: https://doi.org/10.1101/478735
+"melanoma_proteomics"
+
+#' Raw RNA-seq read counts from the B-Cell
+#' melanoma induction study by Griss et al.
+#'
+#' The data is available as a \code{\link[edgeR]{DGEList}} object containing
+#' read counts per gene.
+#'
+#' @author Johannes Griss \email{johannes.griss@meduniwien.ac.at}
+#' @references Griss et al., bioRxiv 478735; doi: https://doi.org/10.1101/478735
+"melanoma_rnaseq"
